c4d48508d9534c3c1b6da176221a68cd470c7577
CivicRebel <milcu.mihai99@yahoo.com>
2021-04-06 13:50:29 +0300

    Initial application structure with database connection string and models

---
+++ SEProject/SEProject/Models/Assignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEProject.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public Course Course { get; set; }
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+
+    }
+}
---
+++ SEProject/SEProject/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SEProject.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+        
+        [Required]
+        public ApplicationUser Teacher { get; set; }
+
+        public IEnumerable<StudentCourse> StudentsEnrolled { get; set; }
+
+    }
+}
